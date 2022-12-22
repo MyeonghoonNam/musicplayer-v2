2b23702856825c537d8bc98d1ce125e81caf1696
MyeongHoonNam <codenmh0822@naver.com>
2022-12-22 23:26:30 +0900

    feat : eslint 설정을 통한 코드 업데이트

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }: AppProps) => {
+	return <Component {...pageProps} />;
+};
+
+export default App;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="ko">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+const Document = () => {
+	return (
+		<Html lang="ko">
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+};
+
+export default Document;
