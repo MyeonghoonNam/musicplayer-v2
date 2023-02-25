6a81c3cc43ce2e8a8ad77c7d0788df09a16b72c8
MyeongHoonNam <codenmh0822@naver.com>
2023-02-25 22:59:47 +0900

    refactor : 절대경로 설정 수정

--- .storybook/main.js
+++ .storybook/main.js
@@ -27,10 +27,7 @@ module.exports = {
     config.resolve.alias = {
       ...config.resolve.alias,
 
-      '@components': path.resolve(__dirname, '../src/components'),
-      '@hooks': path.resolve(__dirname, '../src/hooks'),
-      '@pages': path.resolve(__dirname, '../src/pages'),
-      '@styles': path.resolve(__dirname, '../src/styles'),
+      '@': path.resolve(__dirname, '../src'),
     };
 
     return config;
