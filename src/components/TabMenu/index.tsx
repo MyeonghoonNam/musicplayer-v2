add8bdc27de8fe97bf1a6133c466c1e34dd3b2f8
MyeongHoonNam <codenmh0822@naver.com>
2023-03-07 21:14:35 +0900

    feat : menu 상태에 따른 컴포넌트 UI 렌더링 적용

--- src/components/TabMenu/index.tsx
+++ src/components/TabMenu/index.tsx
@@ -1,22 +1,68 @@
 import Image from 'next/image';
+import type { Menu } from './types';
 
-const TabMenu = () => {
+interface Props {
+  menu: Menu;
+}
+
+const TabMenu = ({ menu }: Props) => {
   return (
     <nav className="fixed bottom-0 flex w-full h-[80px] justify-around bg-[#F2F2F2]">
-      <div className="flex flex-col justify-center items-center cursor-pointer">
-        <Image src="/icons/music.png" alt="Top 5" width={26} height={26} />
-        <span className="mt-[9px] text-[10px]">Top 5</span>
-      </div>
+      <button
+        type="button"
+        className="flex flex-col justify-center items-center"
+      >
+        <Image
+          src={menu === 'Top 5' ? '/icons/music_on.png' : '/icons/music.png'}
+          alt="Top 5"
+          width={26}
+          height={26}
+        />
+        <span
+          className="mt-[9px] text-[10px]"
+          css={[{ color: menu === 'Top 5' ? '#9B51E0' : '' }]}
+        >
+          Top 5
+        </span>
+      </button>
 
-      <div className="flex flex-col justify-center items-center cursor-pointer">
-        <Image src="/icons/layers.png" alt="Play List" width={26} height={26} />
-        <span className="mt-[9px] text-[10px]">Play List</span>
-      </div>
+      <button
+        type="button"
+        className="flex flex-col justify-center items-center"
+      >
+        <Image
+          src={
+            menu === 'Play List' ? '/icons/layers_on.png' : '/icons/layers.png'
+          }
+          alt="Play List"
+          width={26}
+          height={26}
+        />
+        <span
+          className="mt-[9px] text-[10px]"
+          css={[{ color: menu === 'Play List' ? '#9B51E0' : '' }]}
+        >
+          Play List
+        </span>
+      </button>
 
-      <div className="flex flex-col justify-center items-center cursor-pointer">
-        <Image src="/icons/search.png" alt="Search" width={26} height={26} />
-        <span className="mt-[9px] text-[10px]">Search</span>
-      </div>
+      <button
+        type="button"
+        className="flex flex-col justify-center items-center"
+      >
+        <Image
+          src={menu === 'Search' ? '/icons/search_on.png' : '/icons/search.png'}
+          alt="Search"
+          width={26}
+          height={26}
+        />
+        <span
+          className="mt-[9px] text-[10px]"
+          css={[{ color: menu === 'Search' ? '#9B51E0' : '' }]}
+        >
+          Search
+        </span>
+      </button>
     </nav>
   );
 };
