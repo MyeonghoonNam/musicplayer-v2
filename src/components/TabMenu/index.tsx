4d5df94fd0cffb49b05ecca43bf1612768f809ee
MyeongHoonNam <codenmh0822@naver.com>
2023-03-09 22:59:41 +0900

    feat : menu의 리스트를 활용하여 navigation UI 로직 간소화

--- src/components/TabMenu/index.tsx
+++ src/components/TabMenu/index.tsx
@@ -1,68 +1,60 @@
 import Image from 'next/image';
-import { useState } from 'react';
-
-import type { Menu } from './types';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useMemo } from 'react';
 
 const TabMenu = () => {
-  const [menu, setMenu] = useState<Menu>('Top 5');
+  const { pathname } = useRouter();
+
+  const MENU = useMemo(
+    () => [
+      {
+        id: 'menu1',
+        name: 'Top 5',
+        path: '/',
+        on: '/icons/music_on.png',
+        off: '/icons/music.png',
+      },
+      {
+        id: 'menu2',
+        name: 'Play List',
+        path: '/playlist',
+        on: '/icons/layers_on.png',
+        off: '/icons/layers.png',
+      },
+      {
+        id: 'menu3',
+        name: 'Search',
+        path: '/search',
+        on: '/icons/search_on.png',
+        off: '/icons/search.png',
+      },
+    ],
+    [],
+  );
 
   return (
     <nav className="fixed bottom-0 flex w-full h-[80px] justify-around bg-[#F2F2F2]">
-      <button
-        type="button"
-        className="flex flex-col justify-center items-center"
-      >
-        <Image
-          src={menu === 'Top 5' ? '/icons/music_on.png' : '/icons/music.png'}
-          alt="Top 5"
-          width={26}
-          height={26}
-        />
-        <span
-          className="mt-[9px] text-[10px]"
-          css={[{ color: menu === 'Top 5' ? '#9B51E0' : '' }]}
+      {MENU.map(({ id, name, path, on, off }) => (
+        <Link
+          key={id}
+          href={path}
+          className="flex flex-col justify-center items-center"
         >
-          Top 5
-        </span>
-      </button>
-
-      <button
-        type="button"
-        className="flex flex-col justify-center items-center"
-      >
-        <Image
-          src={
-            menu === 'Play List' ? '/icons/layers_on.png' : '/icons/layers.png'
-          }
-          alt="Play List"
-          width={26}
-          height={26}
-        />
-        <span
-          className="mt-[9px] text-[10px]"
-          css={[{ color: menu === 'Play List' ? '#9B51E0' : '' }]}
-        >
-          Play List
-        </span>
-      </button>
-
-      <button
-        type="button"
-        className="flex flex-col justify-center items-center"
-      >
-        <Image
-          src={menu === 'Search' ? '/icons/search_on.png' : '/icons/search.png'}
-          alt="Search"
-          width={26}
-          height={26}
-        />
-        <span
-          className="mt-[9px] text-[10px]"
-          css={[{ color: menu === 'Search' ? '#9B51E0' : '' }]}
-        >
-          Search
-        </span>
-      </button>
+          <Image
+            src={path === pathname ? on : off}
+            alt="Top 5"
+            width={26}
+            height={26}
+          />
+          <span
+            className="mt-[9px] text-[10px]"
+            css={[{ color: path === pathname ? '#9B51E0' : '' }]}
+          >
+            {name}
+          </span>
+        </Link>
+      ))}
     </nav>
   );
 };
