1d5f9b18a9506a78a9b024c71dd25049f9bd8424
MyeongHoonNam <codenmh0822@naver.com>
2023-03-12 23:21:49 +0900

    feat : top3Musics controller 로직 구현

---
+++ server/src/controllers/musicController.ts
@@ -0,0 +1,13 @@
+import type { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { findTop3Musics } from '../services/musicService';
+
+export const getTop3Musics = async (req: Request, res: Response) => {
+  const top3Musics = findTop3Musics();
+
+  if (top3Musics) {
+    return res.status(StatusCodes.OK).send(top3Musics);
+  }
+
+  return res.status(StatusCodes.BAD_REQUEST).send('잘못된 요청입니다.');
+};
