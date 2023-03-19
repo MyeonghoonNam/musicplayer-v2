d92a1b417dc0c92e6d7c789bd910ba9794833bf4
MyeongHoonNam <codenmh0822@naver.com>
2023-03-19 23:42:02 +0900

    fix : csr 환경에서만 동작하도록 타입 코드 추가

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -4,28 +4,29 @@ import ToastManager from './ToastManager';
 import type { CreateToast } from './types';
 
 class Toast {
-  portal: HTMLElement;
+  portal!: HTMLElement;
 
   createToast!: CreateToast;
 
   constructor() {
     const portalId = 'toast-portal';
-    const portalElement = document.getElementById(portalId);
+    const portalElement =
+      typeof window === 'object' && document.getElementById(portalId);
 
     if (portalElement) {
       this.portal = portalElement;
-    } else {
+    } else if (typeof window === 'object') {
       this.portal = document.createElement('div');
       this.portal.id = portalId;
 
       document.body.appendChild(this.portal);
+
+      const handleBind = (createToast: CreateToast) => {
+        this.createToast = createToast;
+      };
+
+      ReactDOM.render(<ToastManager onBind={handleBind} />, this.portal);
     }
-
-    const handleBind = (createToast: CreateToast) => {
-      this.createToast = createToast;
-    };
-
-    ReactDOM.render(<ToastManager onBind={handleBind} />, this.portal);
   }
 
   show(message: string, duration = 2000) {
