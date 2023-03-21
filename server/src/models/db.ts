1dc613a6a67f6db71897751497c8e793c200a444
MyeongHoonNam <codenmh0822@naver.com>
2023-03-21 16:25:13 +0900

    feat : 통합 DB 은닉화 및 DB init 함수 구현

--- server/src/models/db.ts
+++ server/src/models/db.ts
@@ -1,4 +1,5 @@
 import path, { join } from 'path';
+import fs from 'fs/promises';
 import { Low, JSONFile } from 'lowdb';
 
 import { Music } from '../interfaces/musics';
@@ -7,15 +8,36 @@ export interface Data {
   musics: Music[];
 }
 
+const dirname = path.resolve();
+
 export let db: Low<Data>;
 
-export const createConnection = async () => {
-  const dirname = path.resolve();
+export const initDatabase = async () => {
+  const dbFolderPath = join(dirname, './db');
   const filePath = join(dirname, './db/db.json');
-  console.log(filePath);
+  const dbFolder = await fs.readdir(dbFolderPath).catch(() => undefined);
+  const file = await fs.readFile(filePath).catch(() => undefined);
+
+  if (!dbFolder) {
+    await fs.mkdir(dbFolderPath);
+  }
+
+  if (!file) {
+    const jsonFile = await fs.readFile('./db/data.json', 'utf8');
+    const jsonData = JSON.parse(jsonFile);
+
+    await fs.writeFile(filePath, JSON.stringify(jsonData));
+  }
+
+  return filePath;
+};
+
+export const createConnection = async () => {
+  const filePath = await initDatabase();
   const adapter = new JSONFile<Data>(filePath);
 
   db = new Low(adapter);
 
   await db.read();
+  await db.write();
 };
