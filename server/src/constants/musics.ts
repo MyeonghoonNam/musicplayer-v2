be17b514d0c1f56bb1771fe762aa5b45d1377c15
MyeongHoonNam <codenmh0822@naver.com>
2023-03-21 15:54:34 +0900

    feat : 응답 함수에 상수화 적용

---
+++ server/src/constants/musics.ts
@@ -0,0 +1,3 @@
+export const MUSICS_ERRORS = {
+  SOMETHING_WRONG: 'music을 찾는 도중 문제가 생겼습니다.',
+};
--- server/src/controllers/musicController.ts
+++ server/src/controllers/musicController.ts
@@ -1,13 +1,17 @@
 import type { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { findTop3Musics } from '../services/musicService';
+import { createResponse, createError } from '../utils/responseUtils';
+import { MUSICS_ERRORS } from '../constants/musics';
 
 export const getTop3Musics = async (req: Request, res: Response) => {
   const top3Musics = findTop3Musics();
 
   if (top3Musics) {
-    return res.status(StatusCodes.OK).send(top3Musics);
+    return res.status(StatusCodes.OK).send(createResponse(top3Musics));
   }
 
-  return res.status(StatusCodes.BAD_REQUEST).send('잘못된 요청입니다.');
+  return res
+    .status(StatusCodes.BAD_REQUEST)
+    .send(createError(MUSICS_ERRORS.SOMETHING_WRONG));
 };
