8e15fb483bc557869cfb1624929cad1bcf6cf31b
MyeongHoonNam <codenmh0822@naver.com>
2023-03-22 23:52:53 +0900

    feat : 플레이리스트 음악 추가 service 로직 구현

--- server/src/services/musicService.ts
+++ server/src/services/musicService.ts
@@ -1,7 +1,19 @@
-import { db } from '../models/db';
+import { db, create } from '../models/db';
+import type { Music } from '../interfaces/musics';
 
 export const findTop3Musics = () => {
   const musics = db.data?.musics.sort((a, b) => b.vote - a.vote).slice(0, 3);
 
   return musics;
+};
+
+export const addPlayList = async (id: string) => {
+  const music = db.data?.musics.filter((m) => m.id === id)[0];
+
+  if (music) {
+    db.data?.playlist.push(create<Music>(music));
+    await db.write();
+  }
+
+  return music;
 };
