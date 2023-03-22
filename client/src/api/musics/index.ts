02b4bbe544c00a79c8f3eab99bb3ecdb90684bb3
MyeongHoonNam <codenmh0822@naver.com>
2023-03-23 00:05:44 +0900

    feat : 플레이리스트 음악 추가 api 구현

--- client/src/api/musics/index.ts
+++ client/src/api/musics/index.ts
@@ -4,3 +4,6 @@ import type { Music } from './types';
 const RESOURCE = '/musics';
 
 export const getTop3Musics = () => config.get<Music[]>(`${RESOURCE}/top3`);
+
+export const addPlayList = (id: string) =>
+  config.post<Music>(`${RESOURCE}/playlist/${id}`);
