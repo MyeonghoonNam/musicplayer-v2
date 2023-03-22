1272493cc76674ed89f39a9fb28effe234b7e87b
MyeongHoonNam <codenmh0822@naver.com>
2023-03-22 23:53:30 +0900

    feat : 플레이리스트 추가 api route 구현

--- server/src/routes/musicRouter.ts
+++ server/src/routes/musicRouter.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getTop3Musics } from '../controllers/musicController';
+import { getTop3Musics, addPlayList } from '../controllers/musicController';
 
 const router = express.Router();
 
 router.get('/top3', getTop3Musics);
+router.post('/playlist/:id', addPlayList);
 
 export default router;
