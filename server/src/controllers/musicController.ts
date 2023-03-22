04df8142833ea2fa3eef7c292426588bb6f7c1ec
MyeongHoonNam <codenmh0822@naver.com>
2023-03-22 23:53:13 +0900

    feat : 플레이리스트 음악 추가 controller 로직 구현

--- server/src/controllers/musicController.ts
+++ server/src/controllers/musicController.ts
@@ -1,17 +1,28 @@
 import type { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { findTop3Musics } from '../services/musicService';
+
+import * as musicService from '../services/musicService';
 import { createResponse, createError } from '../utils/responseUtils';
 import { MUSICS_ERRORS } from '../constants/musics';
 
 export const getTop3Musics = async (req: Request, res: Response) => {
-  const top3Musics = findTop3Musics();
+  const top3Musics = musicService.findTop3Musics();
 
-  if (top3Musics) {
-    return res.status(StatusCodes.OK).send(createResponse(top3Musics));
+  return res.status(StatusCodes.OK).send(createResponse(top3Musics));
+};
+
+export const addPlayList = async (req: Request, res: Response) => {
+  const { id: musicId } = req.params;
+
+  if (musicId) {
+    const music = await musicService.addPlayList(musicId);
+
+    if (music) {
+      return res.status(StatusCodes.OK).send(createResponse(music));
+    }
   }
 
   return res
     .status(StatusCodes.BAD_REQUEST)
-    .send(createError(MUSICS_ERRORS.SOMETHING_WRONG));
+    .send(createError(MUSICS_ERRORS.INVALID_MUSIC_ID));
 };
