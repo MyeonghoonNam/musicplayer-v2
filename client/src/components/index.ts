4705a54c2a17a615391ab2af8c98ea334a68077b
MyeongHoonNam <codenmh0822@naver.com>
2023-03-24 20:59:11 +0900

    refactor : components 관심사 분리를 위한 디렉토리 수정

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,8 +1,5 @@
 export { default as RootLayout } from './Layout/RootLayout';
 export { default as TabMenuLayout } from './Layout/TabMenuLayout';
 export { default as ViewSizeWarn } from './ViewSizeWarn';
-export { default as Intro } from './Intro';
 export { default as TabMenu } from './TabMenu';
-export { default as Top3Roof } from './Top3Roof';
-export { default as Top3PlayList } from './Top3PlayList';
 export { default as Toast } from './Toast';
---
+++ client/src/pages/main/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Intro } from './Intro';
+export { default as Top3Roof } from './Top3Roof';
+export { default as Top3PlayList } from './Top3PlayList';
