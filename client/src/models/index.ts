0747f1ba9caa83b36afe39a890433536b47dfb97
MyeongHoonNam <codenmh0822@naver.com>
2023-03-24 21:21:42 +0900

    feat : musics model 구성을 통한 react-query cache 관리

---
+++ client/src/models/index.ts
@@ -0,0 +1,1 @@
+export { default as musicCache } from './musics';
---
+++ client/src/models/musics/index.ts
@@ -0,0 +1,7 @@
+const MUSIC_KEY = 'music';
+
+const musicCache = {
+  getTop3Musics: [MUSIC_KEY, 'top3'] as const,
+};
+
+export default musicCache;
