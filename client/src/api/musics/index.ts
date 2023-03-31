673ec6cf942efc9da135350ffc2eb58fba8eabd7
MyeongHoonNam <codenmh0822@naver.com>
2023-04-01 04:10:55 +0900

    feat : playlist 제거 요청 api 구현

--- client/src/api/musics/index.ts
+++ client/src/api/musics/index.ts
@@ -7,3 +7,7 @@ export const getTop3Musics = () => config.get<Music[]>(`${RESOURCE}/top3`);
 
 export const addPlayList = (id: string) =>
   config.post<Music>(`${RESOURCE}/playlist/${id}`);
+
+export const deletePlayList = async (id: string) => {
+  config.delete(`${RESOURCE}/playlist/${id}`);
+};
