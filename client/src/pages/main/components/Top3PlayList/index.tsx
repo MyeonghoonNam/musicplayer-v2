8ef42bfa84ec2a6e4905d9a3dad1aa525c0dad1b
MyeongHoonNam <codenmh0822@naver.com>
2023-04-01 20:22:39 +0900

    feat : 플레이리스트 음악 추가/삭제 기능 구현

--- client/src/pages/main/components/Top3PlayList/index.tsx
+++ client/src/pages/main/components/Top3PlayList/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo } from 'react';
+import { useCallback } from 'react';
 import { Toast, MusicController } from '@/components';
 import { useTop3Musics, useAddPlayList } from '@/pages/main/hooks';
 
@@ -7,25 +7,37 @@ import MusicCover from './MusicCover';
 import MusicTitle from './MusicTitle';
 import MusicArtists from './MusicArtists';
 
+import { TOAST_DURATION } from './constants';
+
 import * as Styled from './styled';
+import useDeletePlayList from '../../hooks/useDeletePlayList';
 
 const Top3PlayList = () => {
   const { data: musics } = useTop3Musics();
-  const { mutateAsync: addPlayList } = useAddPlayList();
-
-  const toastDuration = useMemo(() => 2000, []);
+  const { mutateAsync: addPlayList, isLoading: addPlayListLoading } =
+    useAddPlayList();
+  const { mutateAsync: deletePlayList, isLoading: deletePlayListLoading } =
+    useDeletePlayList();
 
   const handleControllerAddClick = useCallback(
     async (id: string) => {
       await addPlayList(id);
-      Toast.show('보관함에 추가되었습니다.', toastDuration);
+      Toast.show('보관함에 추가되었습니다.', TOAST_DURATION);
     },
-    [addPlayList, toastDuration],
+    [addPlayList],
+  );
+
+  const handleControllerDeleteClick = useCallback(
+    async (id: string) => {
+      await deletePlayList(id);
+      Toast.show('보관함에 삭제되었습니다.');
+    },
+    [deletePlayList],
   );
 
   return (
     <Styled.Container>
-      {musics?.map(({ id, cover, title, artists }, index) => (
+      {musics?.map(({ id, cover, title, artists, hasPlaylist }, index) => (
         <Styled.ItemContainer key={id}>
           <MusicScore score={index + 1} />
 
@@ -40,12 +52,27 @@ const Top3PlayList = () => {
             </Styled.ContentsContainer>
 
             <Styled.ControllerContainer>
-              <MusicController.Play
+              <MusicController
+                mode="play"
+                size="small"
                 onClick={() => handleControllerAddClick(id)}
               />
-              <MusicController.Plus
-                onClick={() => handleControllerAddClick(id)}
-              />
+
+              {hasPlaylist ? (
+                <MusicController
+                  mode="minus"
+                  size="small"
+                  disabled={deletePlayListLoading}
+                  onClick={() => handleControllerDeleteClick(id)}
+                />
+              ) : (
+                <MusicController
+                  mode="plus"
+                  size="small"
+                  disabled={addPlayListLoading}
+                  onClick={() => handleControllerAddClick(id)}
+                />
+              )}
             </Styled.ControllerContainer>
           </Styled.ContentsAndControllerContainer>
         </Styled.ItemContainer>
