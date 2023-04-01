5b906a003877d380f5af845817426f10ee71c292
MyeongHoonNam <codenmh0822@naver.com>
2023-04-01 20:21:18 +0900

    refactor : top3 음악리스트 조회 서비스 로직 함수화

--- server/src/services/musicService.ts
+++ server/src/services/musicService.ts
@@ -10,12 +10,9 @@ export const findPlayListMusic = (id: string) => {
 };
 
 export const findTop3Musics = () => {
-  const playlist = db.data?.playlist;
   const musics = db.data?.musics
     .map((music) =>
-      playlist?.filter(({ id }) => id === music.id).length !== 0
-        ? { ...music, hasPlaylist: true }
-        : music,
+      findPlayListMusic(music.id) ? { ...music, hasPlaylist: true } : music,
     )
     .sort((a, b) => b.vote - a.vote)
     .slice(0, 3);
