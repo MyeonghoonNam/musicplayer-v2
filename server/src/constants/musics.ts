cfdeda571224b67dc692a2722a25b17f7a30b78b
MyeongHoonNam <codenmh0822@naver.com>
2023-04-03 15:46:33 +0900

    feat : playlist not found 상수 추가

--- server/src/constants/musics.ts
+++ server/src/constants/musics.ts
@@ -1,6 +1,7 @@
 export const MUSICS_ERRORS = {
   SOMETHING_WRONG: 'music을 찾는 도중 문제가 생겼습니다.',
   MUSIC_NOT_FOUND: 'music을 찾지 못했습니다.',
+  PLAYLIST_NOT_FOUND: 'playlist를 찾지 못했습니다.',
   INVALID_MUSIC_ID: '잘못된 형식의 music ID 입니다.',
   EXIST_PLAYLIST_MUSIC: '이미 플레이리스트에 존재하는 music입니다.',
 };
