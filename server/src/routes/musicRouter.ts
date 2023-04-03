6e03ac1a1d1913ffe6961a9bab9cc8eebd410ace
MyeongHoonNam <codenmh0822@naver.com>
2023-04-03 15:47:28 +0900

    feat : 플레이리스트 조회 요청 경로 구현

--- server/src/routes/musicRouter.ts
+++ server/src/routes/musicRouter.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import {
   getTop3Musics,
+  getPlayList,
   addPlayList,
   deletePlayList,
 } from '../controllers/musicController';
@@ -8,6 +9,7 @@ import {
 const router = express.Router();
 
 router.get('/top3', getTop3Musics);
+router.get('/playlist', getPlayList);
 router.post('/playlist/:id', addPlayList);
 router.delete('/playlist/:id', deletePlayList);
 
