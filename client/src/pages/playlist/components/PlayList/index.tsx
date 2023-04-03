ee327a78ca2f7ba750d6864db182d8ebc47dd4fc
MyeongHoonNam <codenmh0822@naver.com>
2023-04-03 16:26:48 +0900

    feat : 플레이리스트 페이지 음악 삭제 기능 구현

--- client/src/pages/playlist/components/PlayList/index.tsx
+++ client/src/pages/playlist/components/PlayList/index.tsx
@@ -1,5 +1,8 @@
-import { MusicController } from '@/components';
-import { usePlayList } from '../../hooks';
+import { useCallback } from 'react';
+import { MusicController, Toast } from '@/components';
+
+import { usePlayList, useDeletePlayList } from '../../hooks';
+
 import MusicArtists from './MusicArtists';
 import MusicCover from './MusicCover';
 import MusicTitle from './MusicTitle';
@@ -8,27 +11,39 @@ import * as Styled from './styled';
 
 const PlayList = () => {
   const { data: playlist } = usePlayList();
+  const { mutateAsync: deletePlayList } = useDeletePlayList();
+  const handleDeletePlayListClick = useCallback(
+    async (id: string) => {
+      await deletePlayList(id);
+      Toast.show('보관함에 삭제되었습니다.');
+    },
+    [deletePlayList],
+  );
 
   return (
     <Styled.Container>
-      {playlist?.map((music) => (
-        <Styled.ItemContainer key={music.id}>
+      {playlist?.map(({ id, cover, title, artists }) => (
+        <Styled.ItemContainer key={id}>
           <Styled.ContentsContainer>
             <MusicCover
-              cover={music.cover}
+              cover={cover}
               alt="music-cover"
               width={50}
               height={50}
             />
 
             <Styled.TitleAndArtistsContainer>
-              <MusicTitle title={music.title} />
-              <MusicArtists artists={music.artists} />
+              <MusicTitle title={title} />
+              <MusicArtists artists={artists} />
             </Styled.TitleAndArtistsContainer>
           </Styled.ContentsContainer>
 
           <Styled.ControllerContainer>
-            <MusicController mode="minus" size="small" />
+            <MusicController
+              mode="minus"
+              size="small"
+              onClick={() => handleDeletePlayListClick(id)}
+            />
           </Styled.ControllerContainer>
         </Styled.ItemContainer>
       ))}
