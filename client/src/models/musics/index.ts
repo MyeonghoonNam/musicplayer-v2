de66066f3b79b56a7a414df96e0dd3205a3bca05
MyeongHoonNam <codenmh0822@naver.com>
2023-04-12 21:11:13 +0900

    feat : 플레이리스트 음악 재생 쿼리 키 추가

--- client/src/models/musics/index.ts
+++ client/src/models/musics/index.ts
@@ -3,6 +3,7 @@ const MUSIC_KEY = 'music';
 const musicCache = {
   getTop3Musics: [MUSIC_KEY, 'top3'] as const,
   getPlayList: [MUSIC_KEY, 'playlist'] as const,
+  getPlayMusic: [MUSIC_KEY, 'play'] as const,
 };
 
 export default musicCache;
