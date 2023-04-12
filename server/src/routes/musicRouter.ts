7dbdeb902391cd381e35e2de47ed1adc8cdfca6c
MyeongHoonNam <codenmh0822@naver.com>
2023-04-12 21:10:41 +0900

    feat : 플레이리스트 재생 음악 조회 api route 추가

--- server/src/routes/musicRouter.ts
+++ server/src/routes/musicRouter.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import {
   getTop3Musics,
   getPlayList,
+  getPlayMusic,
   addPlayList,
   deletePlayList,
 } from '../controllers/musicController';
@@ -10,6 +11,7 @@ const router = express.Router();
 
 router.get('/top3', getTop3Musics);
 router.get('/playlist', getPlayList);
+router.get('/playlist/:id', getPlayMusic);
 router.post('/playlist/:id', addPlayList);
 router.delete('/playlist/:id', deletePlayList);
 
