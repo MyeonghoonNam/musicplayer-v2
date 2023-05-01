6a6e35375abbf6e6f3f7a9b977c3bc043a6c74fd
MyeongHoonNam <codenmh0822@naver.com>
2023-05-01 16:53:58 +0900

    feat : SearchBar 하위 컴포넌트 Input 컴포넌트 구현

---
+++ client/src/pages/search/components/SearchBar/Input/index.tsx
@@ -0,0 +1,11 @@
+const Input = () => {
+  return (
+    <input
+      className="w-full h-[36px] pl-[24px] pr-[48px] rounded-[35px] bg-[#f2f2f2]"
+      type="text"
+      placeholder="Search"
+    />
+  );
+};
+
+export default Input;
