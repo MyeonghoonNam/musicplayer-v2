5eafc67501ad6286d5309317520ebf8b6cb705d7
MyeongHoonNam <codenmh0822@naver.com>
2023-05-03 18:26:36 +0900

    refactor : 음악 재생 query key 의존성 deps 추가

--- client/src/models/musics/index.ts
+++ client/src/models/musics/index.ts
@@ -3,7 +3,7 @@ const MUSIC_KEY = 'music';
 const musicCache = {
   getTop3Musics: [MUSIC_KEY, 'top3'] as const,
   getPlayList: [MUSIC_KEY, 'playlist'] as const,
-  getPlayMusic: [MUSIC_KEY, 'play'] as const,
+  getPlayMusic: (id: string) => [MUSIC_KEY, 'play', id] as const,
 };
 
 export default musicCache;
