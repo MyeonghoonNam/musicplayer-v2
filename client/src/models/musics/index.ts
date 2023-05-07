c94d5211c6f3b005f7bba024c4d9bf2440085002
MyeongHoonNam <codenmh0822@naver.com>
2023-05-07 16:43:56 +0900

    feat : 음악 검색 query key 추가

--- client/src/models/musics/index.ts
+++ client/src/models/musics/index.ts
@@ -4,6 +4,7 @@ const musicCache = {
   getTop3Musics: [MUSIC_KEY, 'top3'] as const,
   getPlayList: [MUSIC_KEY, 'playlist'] as const,
   getPlayMusic: (id: string) => [MUSIC_KEY, 'play', id] as const,
+  getSearchPlayList: (query: string) => [MUSIC_KEY, 'search', query] as const,
 };
 
 export default musicCache;
