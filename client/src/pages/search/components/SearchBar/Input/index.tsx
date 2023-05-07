b5f903e06c8ead2a0b353207607a3275e5a4da7d
MyeongHoonNam <codenmh0822@naver.com>
2023-05-07 16:39:28 +0900

    feat : 키워드 입력에 따른 음악 검색 이벤트 구혀

--- client/src/pages/search/components/SearchBar/Input/index.tsx
+++ client/src/pages/search/components/SearchBar/Input/index.tsx
@@ -1,9 +1,24 @@
+import { useRecoilState } from 'recoil';
+import { useCallback, ChangeEvent } from 'react';
+import { searchKeywordState } from '@/store/state';
+
 const Input = () => {
+  const [keyword, setKeyword] = useRecoilState(searchKeywordState);
+
+  const handleChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setKeyword(() => e.target.value);
+    },
+    [setKeyword],
+  );
+
   return (
     <input
       className="w-full h-[36px] pl-[24px] pr-[48px] rounded-[35px] bg-[#f2f2f2]"
       type="text"
       placeholder="Search"
+      value={keyword}
+      onChange={handleChange}
     />
   );
 };
