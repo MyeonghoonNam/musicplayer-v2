99b9398ca675142087b6f8d5826d777a81ad480a
MyeongHoonNam <codenmh0822@naver.com>
2023-05-07 20:27:15 +0900

    feat : debounce를 활용하여 api 호출 최적화 구현

--- client/src/pages/search/components/SearchBar/Input/index.tsx
+++ client/src/pages/search/components/SearchBar/Input/index.tsx
@@ -1,23 +1,27 @@
-import { useRecoilState } from 'recoil';
-import { useCallback, ChangeEvent } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { useState, useCallback, ChangeEvent, useEffect } from 'react';
 import { searchKeywordState } from '@/store/state';
+import { useDebounce } from '@/hooks';
 
 const Input = () => {
-  const [keyword, setKeyword] = useRecoilState(searchKeywordState);
+  const [value, setValue] = useState('');
+  const debouncedValue = useDebounce(value, 500);
+  const setKeyword = useSetRecoilState(searchKeywordState);
 
-  const handleChange = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      setKeyword(() => e.target.value);
-    },
-    [setKeyword],
-  );
+  const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setValue(() => e.target.value);
+  }, []);
+
+  useEffect(() => {
+    setKeyword(() => debouncedValue);
+  }, [setKeyword, debouncedValue]);
 
   return (
     <input
       className="w-full h-[36px] pl-[24px] pr-[48px] rounded-[35px] bg-[#f2f2f2]"
       type="text"
       placeholder="Search"
-      value={keyword}
+      value={value}
       onChange={handleChange}
     />
   );
