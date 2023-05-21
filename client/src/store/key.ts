825fc7794923f72a6ec9785a3c18451675e366e8
MyeongHoonNam <codenmh0822@naver.com>
2023-05-21 22:41:47 +0900

    feat : repeat 전역 상태 지정

--- client/src/store/key.ts
+++ client/src/store/key.ts
@@ -1,2 +1,3 @@
 export const SEARCH_KEYWORD = 'SEARCH_KEYWORD';
 export const PLAY_ROTATE = 'PLAY_ROTATE';
+export const PLAY_REPEAT = 'PLAY_REPEAT';
--- client/src/store/state.ts
+++ client/src/store/state.ts
@@ -1,6 +1,6 @@
 import { atom } from 'recoil';
 
-import { SEARCH_KEYWORD, PLAY_ROTATE } from './key';
+import { SEARCH_KEYWORD, PLAY_ROTATE, PLAY_REPEAT } from './key';
 
 export const searchKeywordState = atom({
   key: SEARCH_KEYWORD,
@@ -9,5 +9,10 @@ export const searchKeywordState = atom({
 
 export const playRotateState = atom({
   key: PLAY_ROTATE,
+  default: false,
+});
+
+export const playRepeatState = atom({
+  key: PLAY_REPEAT,
   default: false,
 });
